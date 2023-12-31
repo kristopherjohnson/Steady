5802900f449962ce2fd837458096e966c8c1f0af
Kristopher Johnson <kris@kristopherjohnson.net>
2023-12-31 13:57:33 -0500

    Initial Commit

---
+++ Steady/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Steady
+//
+//  Created by Kristopher Johnson on 12/31/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Steady/SteadyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SteadyApp.swift
+//  Steady
+//
+//  Created by Kristopher Johnson on 12/31/23.
+//
+
+import SwiftUI
+
+@main
+struct SteadyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
